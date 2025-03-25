57a798eb590285b7ba8a2d558872c688781c2317
FentusGames <124464575+FentusGames@users.noreply.github.com>
2025-03-24 17:49:35 -0700

    rm comment

--- src/main/java/core/assets/Textures.java
+++ src/main/java/core/assets/Textures.java
@@ -79,7 +79,6 @@ public class Textures {
 
 		// Upload texture data
 		GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, width, height, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, imageData);
-		// GL11.glGenerateMipmap(GL11.GL_TEXTURE_2D);
 
 		// Unbind
 		GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
