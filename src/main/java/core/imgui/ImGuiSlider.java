85298fc8ff0b7a0ed2d4f78074920217a17d3ac8
FentusGames <124464575+FentusGames@users.noreply.github.com>
2025-03-29 11:41:37 -0700

    Small test game

--- src/main/java/core/imgui/ImGuiSlider.java
+++ src/main/java/core/imgui/ImGuiSlider.java
@@ -84,7 +84,7 @@ public class ImGuiSlider implements Updateable, Imguiable {
 		float margin = 10f;
 		int flags = ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoBackground | ImGuiWindowFlags.AlwaysAutoResize;
 
-		DirButton[] buttons = new DirButton[] { new DirButton("Up", 0, 1, windowWidth * 0.5f, margin, 0.5f, 0.0f, "up"), new DirButton("Down", 0, -1, windowWidth * 0.5f, windowHeight - margin, 0.5f, 1.0f, "down"), new DirButton("Left", -1, 0, margin, windowHeight * 0.5f, 0.0f, 0.5f, "left"), new DirButton("Right", 1, 0, windowWidth - margin, windowHeight * 0.5f, 1.0f, 0.5f, "right"), };
+		DirButton[] buttons = new DirButton[] { new DirButton("Up", 0, 1, windowWidth * 0.5f, margin, 0.5f, 0.0f, "up"), new DirButton("Down", 0, -1, windowWidth * 0.5f, windowHeight - margin, 0.5f, 1.0f, "down"), new DirButton("Left", 1, 0, margin, windowHeight * 0.5f, 0.0f, 0.5f, "left"), new DirButton("Right", -1, 0, windowWidth - margin, windowHeight * 0.5f, 1.0f, 0.5f, "right"), };
 
 		for (DirButton btn : buttons) {
 			int nextX = coordX + btn.getDx();
--- src/main/java/core/screens/ScreenGame.java
+++ src/main/java/core/screens/ScreenGame.java
@@ -1,5 +1,7 @@
 package core.screens;
 
+import java.util.Random;
+
 import org.jooq.DSLContext;
 import org.lwjgl.opengl.GL11;
 
@@ -10,6 +12,8 @@ import core.imgui.ImGuiMoney;
 import core.imgui.ImGuiNextDay;
 import core.imgui.ImGuiSlider;
 import imgui.ImGui;
+import imgui.flag.ImGuiCol;
+import imgui.type.ImInt;
 
 public class ScreenGame extends Screen {
 	private final Camera camera;
@@ -19,6 +23,13 @@ public class ScreenGame extends Screen {
 	private ImGuiDays imGuiDays = new ImGuiDays();
 	private ImGuiMoney imGuiMoney = new ImGuiMoney();
 	private ImGuiNextDay imGuiNextDay = new ImGuiNextDay();
+
+	// Lab state
+	private static final int ROW_SIZE = 100;
+	private static final int VISIBLE_AT_ONCE = 10;
+	private final char[] topRow = new char[ROW_SIZE];
+	private final char[] bottomRow = new char[ROW_SIZE];
+	private final ImInt labPage = new ImInt(0); // Current lab view page
 
 	// Variables that need saved.
 	private int saveId;
@@ -33,10 +44,10 @@ public class ScreenGame extends Screen {
 	public void init() {
 		super.init();
 
+		initializeLabNucleotides();
+
 		imGuiSlider.addScreen(0, 0, "Main", ctx -> {
 			ImGui.text("This is the main view!");
-			
-			// ImguiableCTXs
 			imGuiDays.imgui(ctx);
 			imGuiMoney.imgui(ctx);
 			imGuiNextDay.imgui(ctx);
@@ -52,6 +63,7 @@ public class ScreenGame extends Screen {
 
 		imGuiSlider.addScreen(1, 0, "Lab", ctx -> {
 			ImGui.text("This is the lab view!");
+			renderLabView();
 		});
 
 		imGuiSlider.addScreen(-1, 0, "Societal Impact", ctx -> {
@@ -60,10 +72,94 @@ public class ScreenGame extends Screen {
 
 		imGuiSlider.addScreen(0, -1, "Finance", ctx -> {
 			ImGui.text("This is the finance view!");
-
-			// ImguiableCTXs
 			imGuiMoney.imgui(ctx);
 		});
+	}
+
+	private void initializeLabNucleotides() {
+		Random random = new Random();
+		char[] options = { 'A', 'T', 'C', 'G' };
+
+		for (int i = 0; i < ROW_SIZE; i++) {
+			topRow[i] = options[random.nextInt(options.length)];
+
+			char bottom;
+			do {
+				bottom = options[random.nextInt(options.length)];
+			} while (bottom == topRow[i]);
+
+			bottomRow[i] = bottom;
+		}
+	}
+
+	private void renderLabView() {
+		ImGui.text("Nucleotide Display - Page " + (labPage.get() + 1));
+
+		int startIdx = labPage.get() * VISIBLE_AT_ONCE;
+		int endIdx = Math.min(startIdx + VISIBLE_AT_ONCE, ROW_SIZE);
+
+		float totalWidth = VISIBLE_AT_ONCE * 24f; // 20px + 4px spacing
+		float centerX = (ImGui.getWindowWidth() - totalWidth) / 2;
+
+		// Top row
+		ImGui.setCursorPosX(centerX);
+		for (int i = startIdx; i < endIdx; i++) {
+			char base = topRow[i];
+			setColorForBase(base);
+			ImGui.pushID(i);
+			if (ImGui.button(String.valueOf(base), 30, 75)) {
+				System.out.println("Clicked TOP: " + base + " at index " + i);
+			}
+			ImGui.popID();
+			ImGui.popStyleColor();
+			ImGui.sameLine();
+		}
+		ImGui.newLine();
+
+		// Bottom row
+		ImGui.setCursorPosX(centerX);
+		for (int i = startIdx; i < endIdx; i++) {
+			char base = bottomRow[i];
+			setColorForBase(base);
+			ImGui.pushID(i + ROW_SIZE);
+			if (ImGui.button(String.valueOf(base), 30, 75)) {
+				System.out.println("Clicked BOTTOM: " + base + " at index " + i);
+			}
+			ImGui.popID();
+			ImGui.popStyleColor();
+			ImGui.sameLine();
+		}
+		ImGui.newLine();
+
+		// Pagination
+		ImGui.spacing();
+		if (ImGui.button("Prev") && labPage.get() > 0) {
+			labPage.set(labPage.get() - 1);
+		}
+		ImGui.sameLine();
+		if (ImGui.button("Next") && (labPage.get() + 1) * VISIBLE_AT_ONCE < ROW_SIZE) {
+			labPage.set(labPage.get() + 1);
+		}
+		ImGui.text("Showing " + VISIBLE_AT_ONCE + " per row. Total pages: " + (ROW_SIZE / VISIBLE_AT_ONCE));
+	}
+
+	private void setColorForBase(char base) {
+		switch (base) {
+		case 'A':
+			ImGui.pushStyleColor(ImGuiCol.Button, 0.0f, 0.5f, 1.0f, 0.7f);
+			break;
+		case 'T':
+			ImGui.pushStyleColor(ImGuiCol.Button, 1.0f, 1.0f, 0.2f, 0.7f);
+			break;
+		case 'G':
+			ImGui.pushStyleColor(ImGuiCol.Button, 0.0f, 1.0f, 0.0f, 0.7f);
+			break;
+		case 'C':
+			ImGui.pushStyleColor(ImGuiCol.Button, 1.0f, 0.0f, 0.2f, 0.7f);
+			break;
+		default:
+			ImGui.pushStyleColor(ImGuiCol.Button, 0.8f, 0.8f, 0.8f, 0.7f);
+		}
 	}
 
 	@Override
@@ -71,7 +167,6 @@ public class ScreenGame extends Screen {
 		GL11.glEnable(GL11.GL_TEXTURE_2D);
 		GL11.glEnable(GL11.GL_BLEND);
 		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-
 		camera.apply();
 	}
 
